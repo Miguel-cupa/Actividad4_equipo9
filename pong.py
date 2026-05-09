94dd5a4a60992ae6b1f3d3caac09313b7339b602
Miguel-cupa <A01713234@tec.mx>
2026-05-08 18:06:34 -0600

    Documentación corregida

--- pong.py
+++ pong.py
@@ -1,19 +1,5 @@
-"""Pong, classic arcade game.
-
-Exercises
-
-1. Change the colors.
-2. What is the frame rate? Make it faster or slower.
-3. Change the speed of the ball.
-4. Change the size of the paddles.
-5. Change how the ball bounces off walls.
-6. How would you add a computer player?
-6. Add a second ball.
-"""
-
+import turtle
 from random import choice, random
-from turtle import *
-
 from freegames import vector
 
 
@@ -26,6 +12,7 @@ ball = vector(0, 0)
 aim = vector(value(), value())
 state = {1: 0, 2: 0}
 
+
 def move(player, change):
     """Cambia la posición del jugador."""
     state[player] += change
@@ -33,22 +20,22 @@ def move(player, change):
 
 def rectangle(x, y, width, height):
     """Dibuja un rectángulo en (x, y) con el ancho y la altura dados."""
-    up()
-    goto(x, y)
-    down()
-    begin_fill()
+    turtle.up()
+    turtle.goto(x, y)
+    turtle.down()
+    turtle.begin_fill()
     for count in range(2):
-        forward(width)
-        left(90)
-        forward(height)
-        left(90)
-    end_fill()
+        turtle.forward(width)
+        turtle.left(90)
+        turtle.forward(height)
+        turtle.left(90)
+    turtle.end_fill()
 
 
 def draw():
     """Dibuja el juego y mueve la pelota de pong."""
-    fillcolor('blue') #El color de las pelotas es de color azul
-    clear()
+    turtle.fillcolor('blue')  # El color de las paletas es azul
+    turtle.clear()
     rectangle(-200, state[1], 10, 50)
     rectangle(190, state[2], 10, 50)
 
@@ -56,10 +43,10 @@ def draw():
     x = ball.x
     y = ball.y
 
-    up()
-    goto(x, y)
-    dot(10)
-    update()
+    turtle.up()
+    turtle.goto(x, y)
+    turtle.dot(10)
+    turtle.update()
 
     if y < -200 or y > 200:
         aim.y = -aim.y
@@ -82,16 +69,16 @@ def draw():
         else:
             return
 
-    ontimer(draw, 50)
+    turtle.ontimer(draw, 50)
 
 
-setup(420, 420, 370, 0)
-hideturtle()
-tracer(False)
-listen()
-onkey(lambda: move(1, 20), 'w')
-onkey(lambda: move(1, -20), 's')
-onkey(lambda: move(2, 20), 'i')
-onkey(lambda: move(2, -20), 'k')
+turtle.setup(420, 420, 370, 0)
+turtle.hideturtle()
+turtle.tracer(False)
+turtle.listen()
+turtle.onkey(lambda: move(1, 20), 'w')
+turtle.onkey(lambda: move(1, -20), 's')
+turtle.onkey(lambda: move(2, 20), 'i')
+turtle.onkey(lambda: move(2, -20), 'k')
 draw()
-done()
+turtle.done()
